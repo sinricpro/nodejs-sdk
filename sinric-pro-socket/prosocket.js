0a03c34b4ae7dfa48c85c2f73f88de692ea34cc9
imdhanush <dhanushdazz@gmail.com>
2019-09-24 23:23:53 +0530

    Connect to sinricPro

---
+++ sinric-pro-socket/prosocket.js
@@ -0,0 +1,12 @@
+const Websocket = require('ws');
+const {appKey,secretKey, deviceId} = require('../credentials');
+const client = new Websocket("ws://ws.sinric.pro",{
+headers:{
+    appkey: appKey,
+    deviceids: deviceId
+}
+});
+
+client.on('open',()=>{
+    console.log('Connected to sinric pro')
+})
