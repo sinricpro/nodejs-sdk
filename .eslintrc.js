17bb36dda40a9234e7e28a2056c3aa250daf2e80
imdhanush <dhanushdazz@gmail.com>
2019-09-27 18:56:01 +0530

    Added promise for signature

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,6 @@
-module.exports = { "extends": "airbnb-base" };
+module.exports = { 
+"extends": "airbnb-base" ,
+rules:{
+    'no-console': 'off',
+    'max-len': 'off'
+}};
--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,6 @@
 const app = require('../index');
 
 function powerState(deviceId, state) {
-  // eslint-disable-next-line no-console
   console.log(deviceId, state);
 }
 
