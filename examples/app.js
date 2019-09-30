0914b518d5c7846474a7e9fef5ca981bf7a99290
imdhanush <dhanushdazz@gmail.com>
2019-10-01 00:33:50 +0530

    Verify successful callback

--- examples/app.js
+++ examples/app.js
@@ -2,40 +2,50 @@ const app = require('../index');
 
 function setPowerState(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 function setPowerLevel(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function adjustPowerLevel(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function setColor(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function setRangeValue(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 function setLockState(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function setBrightness(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function setVolume(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function adjustVolume(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 function setMute(deviceId, data) {
   console.log(deviceId, data);
+  return true;
 }
 
 
