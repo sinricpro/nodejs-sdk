2c645974e4460e7671c23f56decc8ca599592d53
imdhanush <dhanushdazz@gmail.com>
2019-10-13 17:28:15 +0530

    All events added

--- examples/app.js
+++ examples/app.js
@@ -71,5 +71,16 @@ setInterval(() => {
   // raiseEvent(eventNames.powerLevel, 'deviceId', { powerLevel: 44 });
   // raiseEvent(eventNames.color, 'DeviceId', { color: { b: 0, g: 0, r: 0 } });
   // raiseEvent(eventNames.colorTemperature, 'Deviceid', { colorTemperature: 8 });
-  raiseEvent(eventNames.doorBell, 'DeviceId', { state: 'pressed' });
+  // raiseEvent(eventNames.doorBell, 'DeviceId', { state: 'pressed' });
+  // raiseEvent(eventNames.thermostatMode, 'DeviceId', { thermostatMode: 'AUTO' });
+  // raiseEvent(eventNames.rangvalue, 'DeviceId', { rangvalue: 3 });
+  // raiseEvent(eventNames.motion, 'DeviceId', { state: 'detected' });
+  // raiseEvent(eventNames.contact, 'DeviceId', { state: 'closed' });
+  // raiseEvent(eventNames.setVolume, 'DeviceId', { volume: 29 });
+  // raiseEvent(eventNames.selectInput, 'DeviceId', { input: 'HDMI' });
+  // raiseEvent(eventNames.media, 'DeviceId', { control: 'FastForward' });
+  // raiseEvent(eventNames.channel, 'DeviceId', { channel: { name: 'HBO' } });
+  // raiseEvent(eventNames.mode, 'DeviceId', { mode: 'MOVIE' });
+  // raiseEvent(eventNames.lock, 'DeviceId', { state: 'LOCKED' });
+  raiseEvent(eventNames.mute, 'DeviceId', { mute: true });
 }, 2000);
