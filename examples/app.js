de0a4ea5a7d63bdf928837a016b46419f1f390e6
imdhanush <dhanushdazz@gmail.com>
2019-10-22 20:47:37 +0530

    Pass secretKey from toplevel

--- examples/app.js
+++ examples/app.js
@@ -1,51 +1,53 @@
-const { deviceId1, deviceId2 } = require('../credential');
-const { SinricPro, raiseEvent, eventNames } = require('../index');
+const { appKey, deviceId, secretKey } = require('../credentials');
+const {
+  SinricPro, SinricProActions, raiseEvent, eventNames,
+} = require('../index');
 
-function setPowerState(deviceId, data) {
-  console.log(deviceId, data);
+function setPowerState(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
-function setPowerLevel(deviceId, data) {
-  console.log(deviceId, data);
-  return true;
-}
-
-function adjustPowerLevel(deviceId, data) {
-  console.log(deviceId, data);
+function setPowerLevel(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
-function setColor(deviceId, data) {
-  console.log(deviceId, data);
+function adjustPowerLevel(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
-function setRangeValue(deviceId, data) {
-  console.log(deviceId, data);
-  return true;
-}
-function setLockState(deviceId, data) {
-  console.log(deviceId, data);
+function setColor(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
-function setBrightness(deviceId, data) {
-  console.log(deviceId, data);
+function setRangeValue(deviceid, data) {
+  console.log(deviceid, data);
+  return true;
+}
+function setLockState(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
-function setVolume(deviceId, data) {
-  console.log(deviceId, data);
+function setBrightness(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
-function adjustVolume(deviceId, data) {
-  console.log(deviceId, data);
+function setVolume(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
-function setMute(deviceId, data) {
-  console.log(deviceId, data);
+function adjustVolume(deviceid, data) {
+  console.log(deviceid, data);
+  return true;
+}
+
+function setMute(deviceid, data) {
+  console.log(deviceid, data);
   return true;
 }
 
@@ -63,24 +65,26 @@ const callbacks = {
   setMute,
 };
 
-SinricPro(callbacks);
+const sinricpro = new SinricPro(appKey, deviceId, secretKey);
+
+SinricProActions(sinricpro, callbacks);
 
 setInterval(() => {
-  // raiseEvent(eventNames.powerState, deviceId1, { state: 'On' });
-  // raiseEvent(eventNames.setBrightness, 'deviceId', { brightness: 44 });
-  // raiseEvent(eventNames.powerLevel, 'deviceId', { powerLevel: 44 });
-  // raiseEvent(eventNames.color, 'DeviceId', { color: { b: 0, g: 0, r: 0 } });
-  // raiseEvent(eventNames.colorTemperature, 'Deviceid', { colorTemperature: 8 });
-  // raiseEvent(eventNames.doorBell, 'DeviceId', { state: 'pressed' });
-  // raiseEvent(eventNames.thermostatMode, 'DeviceId', { thermostatMode: 'AUTO' });
-  // raiseEvent(eventNames.rangvalue, 'DeviceId', { rangvalue: 3 });
-  // raiseEvent(eventNames.motion, 'DeviceId', { state: 'detected' });
-  // raiseEvent(eventNames.contact, 'DeviceId', { state: 'closed' });
-  // raiseEvent(eventNames.setVolume, 'DeviceId', { volume: 29 });
-  // raiseEvent(eventNames.selectInput, 'DeviceId', { input: 'HDMI' });
-  // raiseEvent(eventNames.media, 'DeviceId', { control: 'FastForward' });
-  // raiseEvent(eventNames.channel, 'DeviceId', { channel: { name: 'HBO' } });
-  // raiseEvent(eventNames.mode, 'DeviceId', { mode: 'MOVIE' });
-  // raiseEvent(eventNames.lock, 'DeviceId', { state: 'LOCKED' });
-  // raiseEvent(eventNames.mute, 'DeviceId', { mute: true });
+  // raiseEvent(sinricpro, eventNames.powerState, 'deviceId', { state: 'On' });
+  // raiseEvent(sinricpro, eventNames.setBrightness, 'deviceId', { brightness: 44 });
+  // raiseEvent(sinricpro, eventNames.powerLevel, 'deviceId', { powerLevel: 44 });
+  // raiseEvent(sinricpro, eventNames.color, 'DeviceId', { color: { b: 0, g: 0, r: 0 } });
+  // raiseEvent(sinricpro, eventNames.colorTemperature, 'Deviceid', { colorTemperature: 8 });
+  // raiseEvent(sinricpro, eventNames.doorBell, 'DeviceId', { state: 'pressed' });
+  // raiseEvent(sinricpro, eventNames.thermostatMode, 'DeviceId', { thermostatMode: 'AUTO' });
+  // raiseEvent(sinricpro, eventNames.rangvalue, 'DeviceId', { rangvalue: 3 });
+  // raiseEvent(sinricpro, eventNames.motion, 'DeviceId', { state: 'detected' });
+  // raiseEvent(sinricpro, eventNames.contact, 'DeviceId', { state: 'closed' });
+  // raiseEvent(sinricpro, eventNames.setVolume, 'DeviceId', { volume: 29 });
+  // raiseEvent(sinricpro, eventNames.selectInput, 'DeviceId', { input: 'HDMI' });
+  // raiseEvent(sinricpro, eventNames.media, 'DeviceId', { control: 'FastForward' });
+  // raiseEvent(sinricpro, eventNames.channel, 'DeviceId', { channel: { name: 'HBO' } });
+  // raiseEvent(sinricpro, eventNames.mode, 'DeviceId', { mode: 'MOVIE' });
+  // raiseEvent(sinricpro, eventNames.lock, 'DeviceId', { state: 'LOCKED' });
+  // raiseEvent(sinricpro, eventNames.mute, 'DeviceId', { mute: true });
 }, 2000);
