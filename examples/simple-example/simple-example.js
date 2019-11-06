ebd835c65dbbbf38f24f8ad0dbfc761c60ee6387
imdhanush <dhanushdazz@gmail.com>
2019-11-06 10:21:23 +0530

    Version update

--- examples/simple-example/simple-example.js
+++ examples/simple-example/simple-example.js
@@ -34,7 +34,7 @@ const callbacks = {
   setPowerState,
 };
 
-const sinricpro = new SinricPro(appKey, deviceId, secretKey);
+const sinricpro = new SinricPro(appKey, deviceId, secretKey, true);
 
 SinricProActions(sinricpro, callbacks);
 
