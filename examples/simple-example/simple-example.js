94e7848560a89c9a031571fe0f82542aa81989d6
Suhas CV <suhazcsk@gmail.com>
2020-11-10 13:38:20 -0500

    base

--- examples/simple-example/simple-example.js
+++ examples/simple-example/simple-example.js
@@ -1,8 +1,8 @@
-const { SinricPro, SinricProActions, raiseEvent, eventNames, SinricProUdp } = require('sinricpro'); // Use require('sinricpro'); if you are using NPM
+const { SinricPro, SinricProActions, raiseEvent, eventNames, SinricProUdp } = require('../../index'); // Use require('sinricpro'); if you are using NPM
 
-const appKey = '5e89a277-522e-4fc8-a044-43e8e487aebc'; // d89f1***-****-****-****-************
-const secretKey = 'e7de1151-01a6-4e1e-8502-d5cd595cd09d-9c0e4cfe-b066-40bb-ab74-8a3a5b0383b3'; // f44d1d31-1c19-****-****-9bc96c34b5bb-d19f42dd-****-****-****-************
-const device1 = '5faabb287052c524384b872c'; // 5d7e7d96069e275ea9******
+const appKey = ''; // d89f1***-****-****-****-************
+const secretKey = ''; // f44d1d31-1c19-****-****-9bc96c34b5bb-d19f42dd-****-****-****-************
+const device1 = ''; // 5d7e7d96069e275ea9******
 const device2 = ''; // 5d80ac5713fa175e99******
 const deviceId = [device1]
 
